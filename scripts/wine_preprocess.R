d191c2cf172c5823918ed6aab873536535c5116c
KevinatorYu <kevinator.yu@hotmail.com>
2024-03-13 14:48:08 -0700

    updated wine_process.R to include saving of summary statistics

--- scripts/wine_preprocess.R
+++ scripts/wine_preprocess.R
@@ -25,9 +25,18 @@ splitting <- function(input, out_dir) {
   wine_test <- testing(wine_split)
   
   # write training and test data to csv files
-  write_csv(training, paste0(out_dir, "/training.csv"))
-  write_csv(test, paste0(out_dir, "/test.csv"))
+  write_csv(wine_train, paste0(out_dir, "/training.csv"))
+  write_csv(wine_test, paste0(out_dir, "/test.csv"))
   
+  # Create a few EDA summary statistics, save into an RDS file
+  summary_stats <- data.frame(train_nrow = nrow(wine_train),
+                              test_nrow = nrow(wine_test),
+                              num_nas = sum(is.na(wine)))
+  try({
+    dir.create(out_dir)
+  })
+  saveRDS(summary_stats, file = paste0(out_dir, "/summary_stats.rds"))
+
 }
 
 splitting(opt[["--input"]], opt[["--out_dir"]])

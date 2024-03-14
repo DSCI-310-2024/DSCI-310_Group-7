957dc6bf6e2bbe42e0b8efbb30395aa261b41060
xnrxng <xinruixiang@hotmail.com>
2024-03-14 12:49:22 -0700

    updated renv.lock and added 2 inputs for download_data.R

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -1,21 +1,29 @@
 # Author: Rico Chan, Rui Xiang Yu, Kevin Yu
 # Date: 2023 March 10
 
-# This script downloads the red wine quality dataset and the basic EDA required
-# from the UCI Machine Learning Repository
+"This script downloads the red wine quality dataset and the basic EDA required 
+from the UCI Machine Learning Repository
 
-# Usage: Rscript download_data.R
+ Usage: scripts/download_data.R --input=<input> --out_dir=<out_dir>
+ 
+ Options:
+ --input=<input>       URL to raw data (https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv)
+--out_dir=<out_dir>    Path to directory where the raw data should be stored as a .csv file
+ " -> doc
 
 library(tidyverse)
+library(docopt)
 
-main <- function() {
-  wine_raw <- read_delim("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv", delim = ";")
+opt <- docopt(doc)
+
+main <- function(input, out_dir) {
+  wine_raw <- read_delim(toString(input), delim = ";")
   new_names <- c("fixed_acidity", "volatile_acidity", "citric_acid",
                  "residual_sugar", "chlorides", "free_sulfur_dioxide",
                  "total_sulfur_dioxide", "density", "pH", "sulphates",
                  "alcohol", "quality")
   colnames(wine_raw) <- new_names
-  write_csv(wine_raw, "data/wine_raw.csv")
+  write_csv(wine_raw, paste0(out_dir, "/wine_raw.csv"))
 }
 
-main()
+main(opt$input, opt$out_dir)

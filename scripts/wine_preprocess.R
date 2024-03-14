2a73952dc285a5cbe9a8fa469a8523cce9225f20
KevinatorYu <kevinator.yu@hotmail.com>
2024-03-14 10:11:20 -0700

    working wine_preprocess.R

--- scripts/wine_preprocess.R
+++ scripts/wine_preprocess.R
@@ -30,7 +30,7 @@ main <- function(input, out_dir) {
   
   # write training and test data to csv files
   write_csv(wine_train, paste0(out_dir, "/training.csv"))
-  write_csv(wine_test, paste0(out_dir, "/test.csv"))
+  write_csv(wine_test, paste0(out_dir, "/testing.csv"))
 }
 
-main(opt$input, opt$out_dir)
+splitting(opt$input, opt$out_dir)

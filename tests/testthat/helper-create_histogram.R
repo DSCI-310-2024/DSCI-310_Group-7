b477b031fcca240cbf7ceb4cb51bccd17d042aad
KevinatorYu <kevinator.yu@hotmail.com>
2024-04-02 10:08:05 -0700

    ok fix fr now

--- tests/testthat/helper-create_histogram.R
+++ tests/testthat/helper-create_histogram.R
@@ -1,22 +1,19 @@
 library(tidyverse)
-
+source("../../R/create_histogram.R")
 # creating a simple dataset
 
 simple_dataset <- tibble(speed = cars$speed,
                          group = c(rep("A", 5), rep("B", 10), rep("C", 10), rep("D", 10), rep("E", 10), rep("F", 5)))
+simple_dataset
 
-# creating a simple histogram, values all filled in
-simple_histogram <- ggplot(aes(x = speed), data = simple_dataset) +
-  geom_histogram(aes(color = group, fill = group)) + 
-  xlab("Speed") +
-  ylab("Count") +
-  theme(text = element_text(size=12.5),
-        legend.position="none")
+# creating a simple histogram
+simple_histogram <- create_histogram(df = simple_dataset,
+                                     x = speed,
+                                     group = group,
+                                     x_lab = "Speed",
+                                     y_lab = "Count",
+                                     font_size = 12.5)
 
-# creating a simple histogram with default values in function
-simple_histogram_default_settings <- ggplot(aes(x = speed), data = simple_dataset) +
-  geom_histogram(aes(color = group, fill = group)) +
-  xlab("") +
-  ylab("") +
-  theme(text = element_text(size=10.5),
-        legend.position="none")
+simple_histogram_default_settings <- create_histogram(df = simple_dataset,
+                                                      x = speed,
+                                                      group = group)
